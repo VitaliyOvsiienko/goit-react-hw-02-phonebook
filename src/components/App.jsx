f85de167373450ac654c1a6f2ecd3ea75edd1d5a
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-05-13 11:35:42 +0200

    Add App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,72 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import { nanoid } from "nanoid";
+import { Container, Title, SubTitle } from "./App.styled";
+
+
+
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
 };
+
+// onSubmitHandler = (newContact) => {
+//   const { contacts } = this.state;
+//   const isDuplicateName = contacts.some(contact =>
+//     contact.name.toLowerCase() === newContact.name.toLowerCase()
+//   );
+
+//   if (isDuplicateName) {
+//     return alert(`${newContact.name} is already in contacts`);
+//   }
+  
+//   const newContactId = nanoid();
+
+//   const updatedContacts = [...contacts, { ...newContact, id: newContactId }];
+//   this.setState({
+//     contacts: updatedContacts
+//   });
+// };
+
+
+onSubmitHandler = (newContact) => {
+  const { contacts } = this.state;
+
+  const isDuplicateName = contacts.some(contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+  );
+
+  if (isDuplicateName) {
+    return alert(`${newContact.name} is already in contacts`);
+  };
+
+  const newContactId = nanoid();
+
+  const updatedContacts = [...contacts, { ...newContact, id: newContactId }];
+  this.setState({
+    contacts: updatedContacts
+  });
+
+};
+
+//  deleteContact = (contactId) => {
+//     this.setState((prev) => ({
+//       contacts: prev.contacts.filter(contact => contact.id !== contactId),
+//     }));
+//   };
+
+
+onDeleteHandler = (id) => {
+  this.setState((prev) => ({
+    contacts: prev.contacts.filter(contact => contact.id !== id)
+  }))
+}
+
+
+
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+    display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        font-size: 24px;
+        color: black;
+
+        padding-left: 15px;
+`;
+
+export const Title = styled.h1`
+    text-transform: uppercase;
+        margin-top: 25px;
+        margin-bottom: 25px;
+`;
+
+export const SubTitle = styled.h2`
+     margin-top: 25px;
+        margin-bottom: 25px;
+`;
