06807eb898134b53e0ee784f91ee58e4498f6ec7
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-05-14 10:13:16 +0200

    add contactForm

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,72 +1,68 @@
-import { Component } from "react";
+// import { Component } from "react";
 // import { nanoid } from "nanoid";
 // import { Container, Title, SubTitle } from "./App.styled";
+// import { ContactForm } from "./ContactForm/ContactForm";
 
-
-
-
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
-};
-
-// onSubmitHandler = (newContact) => {
-//   const { contacts } = this.state;
-//   const isDuplicateName = contacts.some(contact =>
-//     contact.name.toLowerCase() === newContact.name.toLowerCase()
-//   );
-
-//   if (isDuplicateName) {
-//     return alert(`${newContact.name} is already in contacts`);
-//   }
-  
-//   const newContactId = nanoid();
-
-//   const updatedContacts = [...contacts, { ...newContact, id: newContactId }];
-//   this.setState({
-//     contacts: updatedContacts
-//   });
-// };
-
-
-// onSubmitHandler = (newContact) => {
-//   const { contacts } = this.state;
-
-//   const isDuplicateName = contacts.some(contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
-//   );
-
-//   if (isDuplicateName) {
-//     return alert(`${newContact.name} is already in contacts`);
+// export class App extends Component {
+//   state = {
+//     contacts: [
+//       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//     ],
+//     filter: '',
 //   };
 
-//   const newContactId = nanoid();
+//   onSubmitHandler = (newContact) => {
+//     const { contacts } = this.state;
 
-//   const updatedContacts = [...contacts, { ...newContact, id: newContactId }];
-//   this.setState({
-//     contacts: updatedContacts
-//   });
+//     const isDuplicateName = contacts.some(contact => contact.name.toLowerCase() === newContact.name.toLowerCase());
 
-// };
+//     if (isDuplicateName) {
+//       return alert(`${newContact.name} is already in contacts`);
+//     }
 
-//  deleteContact = (contactId) => {
+//     const newContactId = nanoid();
+
+//     const updatedContacts = [...contacts, { ...newContact, id: newContactId }];
+//     this.setState({
+//       contacts: updatedContacts
+//     });
+//   };
+
+//   onDeleteHandler = (id) => {
 //     this.setState((prev) => ({
-//       contacts: prev.contacts.filter(contact => contact.id !== contactId),
+//       contacts: prev.contacts.filter(contact => contact.id !== id),
 //     }));
 //   };
 
+//   onFilterContacts = () => {
+//     let filterContact = [];
+//     if (this.state.filter) {
+//       filterContact = this.state.contacts.filter((contact) =>
+//         contact.name.includes(this.state.filter) ||
+//         contact.name.toLowerCase().includes(this.state.filter)
+//       );
+//     } else {
+//       return this.state.contacts;
+//     }
+//     return filterContact;
+//   };
 
-// onDeleteHandler = (id) => {
-//   this.setState((prev) => ({
-//     contacts: prev.contacts.filter(contact => contact.id !== id)
-//   }))
+//   render() {
+//     const { filter } = this.state;
+//     const filterContacts = this.onFilterContacts();
+
+//     return (
+//       <Container>
+//         <Title>Phonebook</Title>
+//         <ContactForm onSubmit={this.onSubmitHandler} />
+
+//         <SubTitle>Contacts</SubTitle>
+//         <Filter value={filter} onChange={this.onFilterContacts} />
+//         <ContactList contacts={filterContacts} onDeleteContact={this.onDeleteHandler} />
+//       </Container>
+//     );
+//   }
 // }
-
-
-
